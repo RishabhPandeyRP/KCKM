0395186a4dd18f128992952b7674e35fa136894c
Rishabh Pandey <115286284+RishabhPandeyRP@users.noreply.github.com>
2024-07-26 17:35:34 +0530

    Update AfterHero.jsx

--- src/components/AfterHero.jsx
+++ src/components/AfterHero.jsx
@@ -1,26 +1,26 @@
 const AfterHero = ({ data }) => {
     return (
-        <div className="w-[100%]">
+        <div className="w-[100%]  border-blue-700 -ml-10">
             {
                 data.map((item) => (
-                    <div className={`flex ${item.style} my-[10%] justify-between`}>
-                        <div className="w-[45%]">
+                    <div className={`flex ${item.style} my-[10%] justify-center items-center mx-auto   border-green-500 w-fit gap-[150px]`}>
+                        <div className="w-[605px] mx-[10%]  border-red-500">
                             <p className="text-[32px] font-[700] mb-[5%]">
                                 {item.heading}
                             </p>
 
-                            <p className="text-[18px] font-[400] w-[70%]">
+                            {/* <p className="text-[18px] font-[400] w-[70%]">
                                 {item.para1}
                             </p>
                             <p className="text-[18px] font-[400] w-[70%] mt-5">
                                 {item.para2}
-                            </p>
+                            </p> */}
                         </div>
 
 
                         <div className=" border-red-500 w-[45%]">
                             <img src={item.image} alt="" srcset="" 
-                            className="w-[200px] rounded-md" />
+                            className="w-[250px] rounded-md" />
                         </div>
                     </div>
                 ))
